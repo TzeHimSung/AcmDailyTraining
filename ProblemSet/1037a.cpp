7ee54f42e455bbd938b90b9944932a57ed5c96d4
JHSeng <zxc98567@gmail.com>
2018-12-05 22:51:22 +0800

    20181205

---
+++ ProblemSet/1037a.cpp
@@ -0,0 +1,46 @@
+//basic
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+#include <cmath>
+#include <array>
+#include <cstdint>
+#include <iterator>
+//STL
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+//define
+#define int int32_t
+#define ll int64_t
+#define dou double
+#define pb push_back
+#define mp make_pair 
+#define fir first
+#define sec second
+#define init(a,b) fill(begin(a),end(a),b)
+#define sot(a,b) sort(a+1,a+1+b)
+#define rep1(i,a,b) for(int i=a;i<=b;i++)
+#define rep0(i,a,b) for(int i=a;i<b;i++)
+#define repa(i,a) for(auto &i:a)
+#define eps 1e-8
+#define inf 0x3f3f3f3f
+//namespace
+using namespace std;
+//header end
+
+int n,ans=0;
+
+int main()
+{
+    scanf("%d",&n);
+    while (n) ans++,n>>=1;
+    printf("%d\n",ans);
+    return 0;
+}
