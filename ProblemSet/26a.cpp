bd6ee91a2770aa85dd4c1a393e941a338b5f9d53
JHSeng <zxc98567@gmail.com>
2018-12-13 16:38:49 +0800

    20181213

---
+++ ProblemSet/26a.cpp
@@ -0,0 +1,53 @@
+//basic
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+//STL
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <iterator>
+//define
+#define int int32_t
+#define ll int64_t
+#define dou double
+#define pb push_back
+#define mp make_pair 
+#define fir first
+#define sec second
+#define init(a,b) fill(begin(a),end(a),b)
+#define sot(a,b) sort(a+1,a+1+b)
+#define rep1(i,a,b) for(int i=a;i<=b;i++)
+#define rep0(i,a,b) for(int i=a;i<b;i++)
+#define repa(i,a) for(auto &i:a)
+#define eps 1e-8
+#define inf 0x3f3f3f3f
+//namespace
+using namespace std;
+//header end
+
+const int maxn=3e3+10;
+int n,f[maxn],ans=0;
+
+int main()
+{
+    init(f,0);
+    scanf("%d",&n);
+    rep1(i,2,n)
+    {
+        rep0(j,2,i)
+            if (i%j==0 && !f[j]) f[i]++;
+        if (f[i]==2) ans++;
+    }
+    printf("%d\n",ans);
+    return 0;
+}
