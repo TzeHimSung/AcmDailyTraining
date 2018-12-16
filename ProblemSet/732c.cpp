9aa49020da08f605c31601706bda010f16484554
JHSeng <zxc98567@gmail.com>
2018-12-16 22:16:42 +0800

    20181216

---
+++ ProblemSet/732c.cpp
@@ -0,0 +1,49 @@
+//basic
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+//STL
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <iterator>
+//define
+#define int int32_t
+#define ll int64_t
+#define dou double
+#define pb push_back
+#define mp make_pair 
+#define fir first
+#define sec second
+#define init(a,b) fill(begin(a),end(a),b)
+#define sot(a,b) sort(a+1,a+1+b)
+#define rep1(i,a,b) for(int i=a;i<=b;i++)
+#define rep0(i,a,b) for(int i=a;i<b;i++)
+#define repa(i,a) for(auto &i:a)
+#define eps 1e-8
+#define inf 0x3f3f3f3f
+//namespace
+using namespace std;
+//header end
+
+ll b,d,s,m,ans=0;
+
+int main()
+{
+    scanf("%lld%lld%lld",&b,&d,&s);
+    m=max(b,max(d,s)); m--;
+    if (m>b) ans+=m-b;
+    if (m>d) ans+=m-d;
+    if (m>s) ans+=m-s;
+    printf("%lld\n",ans);
+    return 0;
+}
