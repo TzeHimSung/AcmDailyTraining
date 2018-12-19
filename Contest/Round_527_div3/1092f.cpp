17b0c2ddbd56e11ab8f5c04f8541ef91085aea04
JHSeng <zxc98567@gmail.com>
2018-12-19 19:29:52 +0800

    20181219

--- Contest/Round_527_div3/1092f.cpp
+++ Contest/Round_527_div3/1092f.cpp
@@ -45,8 +45,8 @@ vector<int>g[maxn];
 void dfs1(int u, int fa)
 {
     dis[u]=dis[fa]+1;
-    sum[u]=a[u];
-    repa(v,g[u])
+    sum[u]=a[u]; //访问到的节点sum值必然没有赋值过
+    repa(v,g[u]) //遍历当前节点的所有子节点
     {
         if (v==fa) continue;
         dfs1(v,u);
@@ -56,9 +56,10 @@ void dfs1(int u, int fa)
 
 void dfs2(int u, int fa, ll s)
 {
-    s=s-2*sum[u]+tot;
+    s=s-2*sum[u]+tot; //计算以当前节点为根的新ans值，更新答案
+    //稍微想一下，新的答案就是s减去当前节点子树sum值，再加上所有节点权值和tot值，为了方便就写成-2*sum[u]+tot了
     ans=max(s,ans);
-    repa(v,g[u])
+    repa(v,g[u]) //继续dfs遍历所有节点，这种做法相当于暴力枚举
     {
         if (v==fa) continue;
         dfs2(v,u,s);
@@ -71,17 +72,17 @@ int main()
     rep1(i,1,n)
     {
         scanf("%d",&a[i]);
-        tot+=a[i];
+        tot+=a[i]; //计算总节点权值，节点编号从1开始
     }
-    rep0(i,1,n)
+    rep0(i,1,n) //n-1条边
     {
         int u,v; scanf("%d%d",&u,&v);
         g[u].pb(v);
         g[v].pb(u);
     }
-    dis[0]=-1;
-    dfs1(1,0);
-    rep1(i,1,n)
+    dis[0]=-1; //0号节点到自己的距离是-1（不存在这个节点）
+    dfs1(1,0); //dfs计算所有子树中根节点加该子树所有儿子节点的值
+    rep1(i,1,n) //先算出以1号节点为根的值
         ans+=1LL*a[i]*dis[i];
     ll tmp=ans;
     repa(v,g[1])
