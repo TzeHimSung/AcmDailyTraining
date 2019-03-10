3787871f31a27924294f0d2aef76e8517cbe6628
JHSeng <zxc98567@gmail.com>
2019-03-10 19:57:36 +0800

    renew B.java

--- Contest/2019_Mar_9th_Rank/B.java
+++ Contest/2019_Mar_9th_Rank/B.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.math.BigInteger;
 
-public class Main {
+public class B {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         int t = cin.nextInt();
