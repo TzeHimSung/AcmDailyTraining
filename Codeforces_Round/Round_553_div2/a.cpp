665ff209d119a6edac0f53ff769c7dc6630f5339
JHSeng <zxc98567@gmail.com>
2019-04-20 10:34:14 +0800

    change memu tree

---
+++ Codeforces_Round/Round_553_div2/a.cpp
@@ -0,0 +1,57 @@
+/* basic header */
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <cstdint>
+#include <climits>
+#include <float.h>
+/* STL */
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <array>
+#include <iterator>
+/* define */
+#define ll long long
+#define dou double
+#define pb emplace_back
+#define mp make_pair
+#define fir first
+#define sec second
+#define init(a,b) fill(begin(a),end(a),b)
+#define sot(a,b) sort(a+1,a+1+b)
+#define rep1(i,a,b) for(int i=a;i<=b;++i)
+#define rep0(i,a,b) for(int i=a;i<b;++i)
+#define repa(i,a) for(auto &i:a)
+#define eps 1e-8
+#define int_inf 0x3f3f3f3f
+#define ll_inf 0x7f7f7f7f7f7f7f7f
+#define lson curPos<<1
+#define rson curPos<<1|1
+/* namespace */
+using namespace std;
+/* header end */
+
+const int maxn = 55;
+char s[maxn], ss[6] = "ACTG\0";
+int n, ans = int_inf;
+
+int main()
+{
+    scanf("%d", &n);
+    scanf("%s", s + 1);
+    rep1(i, 1, n - 3)
+    {
+        int tmp = 0;
+        rep1(j, 0, 3) tmp += abs(ss[j] - s[i + j]);
+        ans = min(ans, tmp);
+    }
+    printf("%d\n", ans);
+    return 0;
+}
