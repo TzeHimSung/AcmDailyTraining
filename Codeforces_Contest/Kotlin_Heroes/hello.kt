6ad1c9444487eb03728449df57b5c9fc78d3b2f8
JHSeng <zxc98567@gmail.com>
2019-05-28 18:52:20 +0800

    20190528

--- Codeforces_Contest/Kotlin_Heroes/hello.kt
+++ Codeforces_Contest/Kotlin_Heroes/hello.kt
@@ -2,6 +2,14 @@ import java.util.Scanner
 
 fun main(args:Array<String>){
     var cin=Scanner(System.`in`)
-    var a=cin.nextInt(),b=cin.nextInt()
-    
+    var y=cin.nextInt()
+    var b=cin.nextInt()
+    var r=cin.nextInt()
+    var minn=minOf(y,b,r)
+    var ans:Int
+    if (minn==r) ans=3*(r-1)
+    else if (minn==b) ans=3*b
+    else if (r>=y+2) ans=3*(y+1)
+    else ans=3*y
+    println(ans)
 }
