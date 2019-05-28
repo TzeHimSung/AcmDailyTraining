791e3fc7dd998bbee90b1410ef24e997b1163ee2
JHSeng <zxc98567@gmail.com>
2019-05-29 01:04:55 +0800

    kotlin

--- Codeforces_Contest/Kotlin_Heroes/hello.kt
+++ Codeforces_Contest/Kotlin_Heroes/hello.kt
@@ -2,14 +2,22 @@ import java.util.Scanner
 
 fun main(args:Array<String>){
     var cin=Scanner(System.`in`)
-    var y=cin.nextInt()
-    var b=cin.nextInt()
-    var r=cin.nextInt()
-    var minn=minOf(y,b,r)
-    var ans:Int
-    if (minn==r) ans=3*(r-1)
-    else if (minn==b) ans=3*b
-    else if (r>=y+2) ans=3*(y+1)
-    else ans=3*y
-    println(ans)
+    var n=cin.nextInt()
+    while (n-->0){
+        var a=cin.nextInt()
+        var b=cin.nextInt()
+        if (a<b){
+            var k=a
+            a=b
+            b=k
+        }
+        var z=1
+        var y=a-b+1
+        var x=a-y
+        print(x)
+        print(" ")
+        print(y)
+        print(" ")
+        println(z)
+    }
 }
