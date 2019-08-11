0c53cf28e7c0cbef00d4c1808976c708f8c21053
JHSeng <zxc98567@163.com>
2019-08-12 02:15:24 +0800

    renew 1005.cpp

--- HDU_MultiSchool/day6/1005.cpp
+++ HDU_MultiSchool/day6/1005.cpp
@@ -27,14 +27,14 @@ struct Point {
 
 struct Node {
     ll sum, maxv, prev, sufv;
-} segt[maxn * 4];
+} segt[maxn << 2];
 
 void maintain(int curpos) {
-    segt[curpos].sum = segt[lson].sum + segt[rson].sum;
-    segt[curpos].maxv = max(segt[lson].maxv, segt[rson].maxv);
-    segt[curpos].maxv = max(segt[curpos].maxv, segt[lson].sufv + segt[rson].prev);
-    segt[curpos].prev = max(segt[lson].prev, segt[lson].sum + segt[rson].prev);
-    segt[curpos].sufv = max(segt[lson].sufv + segt[lson].sum, segt[rson].sufv);
+    segt[curpos].sum = segt[lson].sum + segt[rson].sum; // 总和
+    segt[curpos].maxv = max(segt[lson].maxv, segt[rson].maxv); // 答案
+    segt[curpos].maxv = max(segt[curpos].maxv, segt[lson].sufv + segt[rson].prev); //不拿当前的点
+    segt[curpos].prev = max(segt[lson].prev, segt[lson].sum + segt[rson].prev); // 最大前缀和
+    segt[curpos].sufv = max(segt[lson].sufv + segt[rson].sum, segt[rson].sufv); // 最大后缀和
 }
 
 void build(int curpos, int curl, int curr) {
@@ -50,7 +50,7 @@ void build(int curpos, int curl, int curr) {
 void update(int pos, int curpos, int curl, int curr, ll v) {
     if (curl == curr) {
         segt[curpos].sum += v;
-        segt[curpos].maxv = segt[curpos].prev = segt[curpos].sufv = max(0ll, v);
+        segt[curpos].maxv = segt[curpos].prev = segt[curpos].sufv = max(0ll, segt[curpos].sum); // 不拿就是0
         return;
     }
     int mid = curl + curr >> 1;
@@ -70,21 +70,23 @@ int main() {
         }
         sort(keyX.begin(), keyX.end());
         sort(keyY.begin(), keyY.end());
-        int pp = unique(keyX.begin(), keyX.end()) - keyX.begin();
-        int qq = unique(keyY.begin(), keyY.end()) - keyY.begin();
+        int totalX = unique(keyX.begin(), keyX.end()) - keyX.begin();
+        int totalY = unique(keyY.begin(), keyY.end()) - keyY.begin();
+        // 把点的xy值都离散化
         for (int i = 0; i < n; i++) {
-            p[i].x = lower_bound(keyX.begin(), keyX.begin() + pp, p[i].x) - keyX.begin() + 1;
-            p[i].y = lower_bound(keyY.begin(), keyY.begin() + qq, p[i].y) - keyY.begin() + 1;
+            p[i].x = lower_bound(keyX.begin(), keyX.begin() + totalX, p[i].x) - keyX.begin() + 1;
+            p[i].y = lower_bound(keyY.begin(), keyY.begin() + totalY, p[i].y) - keyY.begin() + 1;
         }
-        sort(p, p + n);
-        for (int i = 0; i < pp; i++) {
+        sort(p, p + n); // 先按x然后按y排序
+        // 遍历totalX条边，相当于枚举矩形左边界
+        for (int i = 0; i < totalX; i++) {
             int x = i + 1, tmp = 0;
-            while (tmp < n && p[tmp].x < x) tmp++;
-            build(1, 1, qq);
-            while (tmp < n) {
+            while (tmp < n && p[tmp].x < x) tmp++; // 找到在当前点在p数组中的位置
+            build(1, 1, totalY); //建立空线段树
+            while (tmp < n) { //扫一遍点, 计算后面所有x相同的点
                 int curx = p[tmp].x;
                 while (tmp < n && p[tmp].x == curx) {
-                    update(p[tmp].y, 1, 1, qq, p[tmp].c);
+                    update(p[tmp].y, 1, 1, totalY, p[tmp].c);
                     tmp++;
                 }
                 ans = max(ans, segt[1].maxv);
