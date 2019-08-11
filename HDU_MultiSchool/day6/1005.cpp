f607cb7e55ffc1064526c84dc8070886ace409eb
JHSeng <zxc98567@gmail.com>
2019-08-12 00:01:09 +0800

    renew 1005.cpp && c.cpp

--- HDU_MultiSchool/day6/1005.cpp
+++ HDU_MultiSchool/day6/1005.cpp
@@ -47,11 +47,23 @@ void build(int curpos, int curl, int curr) {
     maintain(curpos);
 }
 
+void update(int pos, int curpos, int curl, int curr, ll v) {
+    if (curl == curr) {
+        segt[curpos].sum += v;
+        segt[curpos].maxv = segt[curpos].prev = segt[curpos].sufv = max(0ll, v);
+        return;
+    }
+    int mid = curl + curr >> 1;
+    if (pos <= mid) update(pos, lson, curl, mid, v);
+    else update(pos, rson, mid + 1, curr, v);
+    maintain(curpos);
+}
+
 int main() {
     int t; scanf("%d", &t);
     while (t--) {
         vector<int>keyX, keyY;
-        int n; scanf("%d", &n);
+        int n; scanf("%d", &n); ll ans = 0;
         for (int i = 0; i < n; i++) {
             scanf("%d%d%d", &p[i].x, &p[i].y, &p[i].c);
             keyX.pb(p[i].x); keyY.pb(p[i].y);
@@ -65,7 +77,20 @@ int main() {
             p[i].y = lower_bound(keyY.begin(), keyY.begin() + qq, p[i].y) - keyY.begin() + 1;
         }
         sort(p, p + n);
-
+        for (int i = 0; i < pp; i++) {
+            int x = i + 1, tmp = 0;
+            while (tmp < n && p[tmp].x < x) tmp++;
+            build(1, 1, qq);
+            while (tmp < n) {
+                int curx = p[tmp].x;
+                while (tmp < n && p[tmp].x == curx) {
+                    update(p[tmp].y, 1, 1, qq, p[tmp].c);
+                    tmp++;
+                }
+                ans = max(ans, segt[1].maxv);
+            }
+        }
+        printf("%lld\n", ans);
     }
     return 0;
 }
