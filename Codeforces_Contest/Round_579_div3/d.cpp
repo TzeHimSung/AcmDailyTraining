c8b1e8b95e9a60f82aa1150e4b2e5a22661711e9
JHSeng <zxc98567@163.com>
2019-08-14 02:39:21 +0800

    renew d.cpp

--- Codeforces_Contest/Round_579_div3/d.cpp
+++ Codeforces_Contest/Round_579_div3/d.cpp
@@ -24,14 +24,18 @@ int main() {
     int len1 = s.size(), len2 = t.size();
     vector<int>pre(len2), suf(len2);
     int cur = 0;
+    // pre存放从前往后扫时字符串t中的字母在字符串s中出现的位置
     for (int i = 0; cur < len2; i++)
         if (s[i] == t[cur])
             pre[cur++] = i;
+    // suf存放从后往前扫时字符串t中字母在字符串s中出现的位置
     cur = len2;
     for (int i = len1; i--;) {
         if (s[i] == t[cur - 1])
             suf[--cur] = i;
     }
+    // 这样得到的pre数组是字符串t中的每个字母在字符串s中最早出现的下标
+    // suf数组同理，得到的是最晚出现的下标
     int ans = max(len1 - 1 - pre.back(), suf[0]);
     for (int i = 0; i + 1 < len2; i++)
         ans = max(ans, suf[i + 1] - pre[i] - 1);
