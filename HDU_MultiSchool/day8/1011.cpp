cc9487efb8391fd8a7ce32f228f9d51e9eb3934f
JHSeng <zxc98567@163.com>
2019-08-14 17:03:08 +0800

    hdu multischool day8

---
+++ HDU_MultiSchool/day8/1011.cpp
@@ -0,0 +1,73 @@
+/* Codeforces Contest 2019_mutc_08
+ * Problem K
+ * Au: SJoshua
+ */
+#include <queue>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct info {
+    int num, tea;
+};
+
+struct pnum {
+    int id, num;
+    bool operator < (const pnum &num) const {
+        return id < num.id;
+    }
+};
+
+int main(void) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector <info> _class(n);
+        for (int i = 0; i < n; i++) {
+            cin >> _class[i].num >> _class[i].tea;
+        }
+        sort(_class.begin(), _class.end(), [](info & a, info & b) {
+            return a.tea == b.tea ? a.num > b.num : a.tea > b.tea;
+        });
+        int ans = 0;
+        vector <int> jump(n);
+        for (int i = 0; i < n - 1; i++) {
+            jump[i] = i + 1;
+        }
+        jump[n - 1] = 0;
+        for (int i = 0; i < n; i++) {
+            int nxt = i;
+            int sign = 0, round = 0;
+            while (_class[i].num) {
+                round++;
+                int last = nxt;
+                nxt = jump[nxt];
+                if (nxt == i) {
+                    break;
+                }
+                if (_class[nxt].tea) {
+                    int drink = min(_class[nxt].tea, _class[i].num);
+                    _class[i].num -= drink;
+                    _class[nxt].tea -= drink;
+                    ans += drink;
+                    sign += drink;
+                } else {
+                    jump[last] = jump[nxt];
+                }
+                if (round > n) break;
+            }
+            if (!sign && _class[i].num) break;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
