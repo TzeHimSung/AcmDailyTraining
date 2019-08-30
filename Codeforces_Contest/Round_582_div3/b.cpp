fe1741019bd120714a87a6b8fb4490c68cea90d2
JHSeng <zxc98567@gmail.com>
2019-08-31 01:04:44 +0800

    codeforces round 582 div3

--- Codeforces_Contest/Round_582_div3/b.cpp
+++ Codeforces_Contest/Round_582_div3/b.cpp
@@ -17,23 +17,23 @@
 using namespace std;
 /* header end */
 
-const int maxn=150010;
-int n,a[maxn];
+const int maxn = 150010;
+int n, a[maxn];
 
 int main() {
-	int t; scanf("%d",&t);
-	while (t--){
-		scanf("%d",&n);
-		rep1(i,1,n) scanf("%d",&a[i]);
-		stack<int>s; 
-		while (!s.empty()) s.pop();
-		s.push(a[n]);
-		int ans=0;
-		for (int i=n-1;i>0;i--){
-			if (a[i]>s.top()) ans++;
-			else if (a[i]<s.top()) s.push(a[i]);
-		}
-		printf("%d\n",ans);
-	}
+    int t; scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        rep1(i, 1, n) scanf("%d", &a[i]);
+        stack<int>s;
+        while (!s.empty()) s.pop();
+        s.push(a[n]);
+        int ans = 0;
+        for (int i = n - 1; i > 0; i--) {
+            if (a[i] > s.top()) ans++;
+            else if (a[i] < s.top()) s.push(a[i]);
+        }
+        printf("%d\n", ans);
+    }
     return 0;
 }
