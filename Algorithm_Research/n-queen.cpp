994c9f55048dad5c26fe499e233b362b6def2061
JHSeng <zxc98567@gmail.com>
2019-10-06 19:33:55 +0800

    renew n-queen.cpp

--- Algorithm_Research/n-queen.cpp
+++ Algorithm_Research/n-queen.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
 #include <utility>
 
 // using standard library
@@ -16,6 +17,7 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+using std::set;
 using std::pair;
 using std::make_pair;
 
@@ -23,7 +25,7 @@ using std::make_pair;
 
 int __chessboardSize;
 vector<vector<int>> __map;
-vector<pair<int, int>> __queenPos;
+set<int> __ansSet;
 
 // function declaration
 
@@ -33,7 +35,7 @@ void solve              ();
 void printChessboard    ();
 bool checkPosition      (int __curr_x, int __curr_y);
 void dfs                (int __filled);
-void fillVis            (int __curr_x, int __curr_y, int __val);
+int  getHash            ();
 
 // function achieve
 
@@ -57,6 +59,8 @@ int convertStringToInt(string __str) {
 // solve n-queen problem
 void solve() {
     // chessboard init
+    __ansSet.clear();
+
     __map.resize(__chessboardSize + 1);
     for (auto &__i : __map) {
         for (int __j = 0; __j <= __chessboardSize; __j++) {
@@ -67,11 +71,11 @@ void solve() {
     for (int __i = 1; __i <= __chessboardSize; __i++) {
         for (int __j = 1; __j <= __chessboardSize; __j++) {
             if (checkPosition(__i, __j)) {
-                fillVis(__i, __j, 1);
-                __queenPos.push_back(make_pair(__i, __j));
+                __map[__i][__j] = 1;
+                // __queenPos.push_back(make_pair(__i, __j));
                 dfs(1);
-                fillVis(__i, __j, 0);
-                __queenPos.pop_back();
+                __map[__i][__j] = 0;
+                // __queenPos.pop_back();
             }
         }
     }
@@ -79,49 +83,25 @@ void solve() {
 
 // dfs
 void dfs(int __filled) {
-    if (__filled == __chessboardSize + 1) {
-        printChessboard();
-        puts("");
+    if (__filled == __chessboardSize) {
+        int __currHash = getHash();
+        if (!__ansSet.count(__currHash)) {
+            __ansSet.insert(__currHash);
+            printChessboard();
+        }
         return;
     }
     for (int __i = 1; __i <= __chessboardSize; __i++) {
         for (int __j = 1; __j <= __chessboardSize; __j++) {
             if (checkPosition(__i, __j)) {
-                fillVis(__i, __j, 1);
-                __queenPos.push_back(make_pair(__i, __j));
+                __map[__i][__j] = 1;
+                // __queenPos.push_back(make_pair(__i, __j));
                 dfs(__filled + 1);
-                fillVis(__i, __j, 0);
-                __queenPos.pop_back();
+                __map[__i][__j] = 0;
+                // __queenPos.pop_back();
             }
         }
     }
-}
-
-// fill map matrix
-void fillVis(int __curr_x, int __curr_y, int __val) {
-    // current point
-    __map[__curr_x][__curr_y] = __val;
-
-    // total column
-    for (int __i = 1; __i <= __chessboardSize; __i++)
-        __map[__i][__curr_y] = __val;
-
-    // total row
-    for (int __j = 1; __j <= __chessboardSize; __j++)
-        __map[__curr_x][__j] = __val;
-
-    // diagonal
-    int __tmp_x = __curr_x, __tmp_y = __curr_y;
-    while (__tmp_x - 1 >= 1 && __tmp_y - 1 >= 1)
-        __tmp_x--, __tmp_y--;
-    while (__tmp_x <= __chessboardSize && __tmp_y <= __chessboardSize)
-        __map[__tmp_x++][__tmp_y++] = __val;
-
-    __tmp_x = __curr_x, __tmp_y = __curr_y;
-    while (__tmp_x - 1 >= 1 && __tmp_y + 1 <= __chessboardSize)
-        __tmp_x--, __tmp_y++;
-    while (__tmp_x <= __chessboardSize && __tmp_y >= 1)
-        __map[__tmp_x++][__tmp_y--] = __val;
 }
 
 bool checkPosition(int __curr_x, int __curr_y) {
@@ -156,19 +136,23 @@ bool checkPosition(int __curr_x, int __curr_y) {
 
 // print current chessboard
 void printChessboard() {
-    for (auto &__i : __map) {
-        for (auto __j : __i) {
-            cout << __j << " ";
+    for (int __i = 1; __i <= __chessboardSize; __i++) {
+        for (int __j = 1; __j <= __chessboardSize; __j++) {
+            cout << __map[__i][__j] << " ";
         }
         cout << endl;
     }
+    cout << "------------------------------------" << endl;
 }
 
-// check current position is useable or not
-bool checkPosition(int __curr_x, int __curr_y) {
-    for (int i = 1;) {
-
+int getHash() {
+    int __ret = 0;
+    for (int __i = 1; __i <= __chessboardSize; __i++) {
+        for (int __j = 1; __j <= __chessboardSize; __j++) {
+            __ret += __i * __j * __map[__i][__j];
+        }
     }
+    return __ret;
 }
 
 int main() {
@@ -183,10 +167,10 @@ int main() {
         cout << "This input is illegal, please input again";
         cin >> inputString;
     }
-    chessboardSize = convertStringToInt(inputString);
+    __chessboardSize = convertStringToInt(inputString);
 
     // solve problem
-    solve(chessboardSize);
+    solve();
 
     return 0;
 }
