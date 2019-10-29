ab3e69d401b31bbc6f33495973d33da9c6a486f6
JHSeng <zxc98567@gmail.com>
2019-10-29 18:32:28 +0800

    renew std

--- Newbie_Cup_Problem/Rocket_testing/std.cpp
+++ Newbie_Cup_Problem/Rocket_testing/std.cpp
@@ -1,3 +1,4 @@
+// problem 1009 std
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -10,7 +11,8 @@ struct Vector {
     ll x, y;
     Vector() {}
     Vector(int a, int b): x(a), y(b) {}
-    // 定义不同向量如何比较大小，通过斜率判断优先级
+    // 重载<号。定义不同向量如何比较大小，通过斜率判断优先级
+    // atan2这个函数返回方位角的弧度，取值范围为(-pi,pi]
     bool operator<(const Vector &rhs)const {
         return atan2(y, x) < atan2(rhs.y, rhs.x);
     }
@@ -22,12 +24,15 @@ int main() {
     cin >> T;
     while (T--) {
         ll ans = 0;
-        int n;
+        int n, p = 0;
         cin >> n;
         for (int i = 1; i <= n; i++) {
-            cin >> a[i].x >> a[i].y;
+            int x, y; cin >> x >> y;
+            if (x || y) a[++p] = Vector(x, y);
+            // 为什么不能x和y同为0，因为待会要利用atan2这个函数进行排序，当x和y同为0时atan2返回undefined
         }
-        // 排序
+        n = p;
+        // 排序，因为Vector重载了<号，所以直接sort即可
         sort(a + 1, a + 1 + n);
         // 依次枚举每个向量作为本次扫描的起点
         for (int start_position = 1; start_position <= n; start_position++) {
