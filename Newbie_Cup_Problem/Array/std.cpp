9351a99ab63118a03da6236a3dbb2811169e49a3
JHSeng <zxc98567@gmail.com>
2019-10-29 20:28:23 +0800

    renew std.cpp

---
+++ Newbie_Cup_Problem/Array/std.cpp
@@ -0,0 +1,30 @@
+/* basic header */
+#include <bits/stdc++.h>
+/* define */
+#define ll long long
+#define dou double
+#define pb emplace_back
+#define mp make_pair
+#define sot(a,b) sort(a+1,a+1+b)
+#define rep1(i,a,b) for(int i=a;i<=b;++i)
+#define rep0(i,a,b) for(int i=a;i<b;++i)
+#define eps 1e-8
+#define int_inf 0x3f3f3f3f
+#define ll_inf 0x7f7f7f7f7f7f7f7f
+#define lson (curpos<<1)
+#define rson (curpos<<1|1)
+/* namespace */
+using namespace std;
+/* header end */
+
+ll f[31];
+
+int main() {
+    f[1] = f[2] = 1;
+    for (int i = 3; i <= 30; i++) f[i] = f[i - 1] + f[i - 2];
+    int t; scanf("%d", &t);
+    while (t--) {
+        int x; cin >> x; cout << f[x] << endl;
+    }
+    return 0;
+}
