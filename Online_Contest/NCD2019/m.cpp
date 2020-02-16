68765e01d9b55022c8d3aa7de1246dcface7fc4e
JHSeng <zxc98567@gmail.com>
2020-02-16 17:35:09 +0800

    update NCD 2019

--- Online_Contest/NCD2019/m.cpp
+++ Online_Contest/NCD2019/m.cpp
@@ -1,33 +1,53 @@
-/* basic header */
 #include <bits/stdc++.h>
-/* define */
-#define ll long long
-#define pb emplace_back
-#define mp make_pair
-#define eps 1e-8
-#define lson (curpos<<1)
-#define rson (curpos<<1|1)
-/* namespace */
 using namespace std;
-/* header end */
+#define ff first
+#define ss second
+typedef long long ll;
+typedef pair<int, int> pii;
 
-int t;
+ll mod = 1e9 + 7;
+ll qp(ll base, ll b) {
+    ll res = 1;
+    while (b) {
+        if (b & 1)
+            res = res * base % mod;
+        base = base * base % mod;
+        b >>= 1;
+    }
+    return res;
+}
 
 int main() {
+    int t;
     scanf("%d", &t);
     while (t--) {
-        double b1, p1, b2, p2;
-        scanf("%lf%lf%lf%lf", &b1, &p1, &b2, &p2);
-        if (b1 * b2 == 0) {
-            if (b1 == b2) puts("Lasy");
-            else if (b1 == 0) puts("Congrats");
-            else puts("HaHa");
+        int b1, p1, b2, p2;
+        scanf("%d%d%d%d", &b1, &p1, &b2, &p2);
+        double tmp = p1 * log(b1) - p2 * log(b2);
+        int l = -1, r = -1;
+        if (!b1)
+            l = 0;
+        else if (!p1)
+            l = 1;
+        if (!b2)
+            r = 0;
+        else if (!p2)
+            r = 1;
+        if (l != -1 && r != -1) {
+            if (l == r)
+                printf("Lazy\n");
+            else if (l < r)
+                printf("Congrats\n");
+            else
+                printf("HaHa\n");
             continue;
         }
-        double tmp = p1 / p2 * (log(b1) / log(b2));
-        if (tmp < 1) puts("Congrats");
-        else if (tmp > 1) puts("HaHa");
-        else puts("Lazy");
+
+        if (fabs(tmp) < 1e-8 && qp(b1, p1) == qp(b2, p2))
+            printf("Lazy\n");
+        else if (tmp < 1e-8)
+            printf("Congrats\n");
+        else
+            printf("HaHa\n");
     }
-    return 0;
 }
