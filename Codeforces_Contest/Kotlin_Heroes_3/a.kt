7dd1ad06ae884c9cba8fa727438a80e74ae62c13
JHSeng <zxc98567@gmail.com>
2020-02-27 22:14:20 +0800

    update Kotlin Heroes 3

--- Codeforces_Contest/Kotlin_Heroes_3/a.kt
+++ Codeforces_Contest/Kotlin_Heroes_3/a.kt
@@ -2,10 +2,33 @@ fun readint()=readLine()!!.toInt()
 fun readll()=readLine()!!.toLong()
 fun readline_int()=(readLine()!!.split(" ").map{it.toInt()}).toIntArray()
 fun readline_ll()=(readLine()!!.split(" ").map{it.toLong()}).toLongArray()
+data class pair(var first:Int, var second:Int)
 
 fun main() {
-	val a = readline_int().sorted()
-	for (i in 0 until 10){
-		println("%d %d %d".format(a[3]-a[0],a[3]-a[1],a[3]-a[2]))
+	var t = readint()
+	for (cnt in 0 until t){
+		var curr=readint()
+		if (0<=curr && curr<1000){
+			println("%d".format(curr))
+		} else if (1000<=curr && curr<1000000){
+			var remain=curr%1000;
+			if (remain<500) {
+				println("%dK".format(curr/1000))
+			} else{
+				var tmp=curr/1000+1
+				if (tmp==1000) {
+					println("1M")
+				} else{
+					println("%dK".format(curr/1000+1))
+				}
+			}
+		} else{
+			var remain=curr%1000000
+			if (remain<500000){
+				println("%dM".format(curr/1000000))
+			} else{
+				println("%dM".format(curr/1000000+1))
+			}
+		}
 	}
 }
