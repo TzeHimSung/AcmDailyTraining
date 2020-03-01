b33f509921e25665c51fbacc9bca3a3658955f2d
JHSeng <zxc98567@gmail.com>
2020-03-01 18:03:03 +0800

    update 20200301

---
+++ Training_log/Other/xianyu_20200301.cpp
@@ -0,0 +1,23 @@
+/* basic header */
+#include <bits/stdc++.h>
+/* define */
+#define ll long long
+#define pb emplace_back
+#define mp make_pair
+#define eps 1e-8
+#define lson (curpos<<1)
+#define rson (curpos<<1|1)
+/* namespace */
+using namespace std;
+/* header end */
+
+vector<pair<int, int>>interval;
+
+int main() {
+    int n;
+    cin >> n;
+    interval.resize(n);
+    for (auto &i : interval) cin >> i.first >> i.second;
+    sort(internal.begin(), internal.end());
+    return 0;
+}
