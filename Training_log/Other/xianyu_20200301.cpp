477570eb5ea05a2f63da79e40163b0a6345e0197
JHSeng <zxc98567@gmail.com>
2020-03-01 18:45:34 +0800

    update XianYu 20200301

--- Training_log/Other/xianyu_20200301.cpp
+++ Training_log/Other/xianyu_20200301.cpp
@@ -11,13 +11,27 @@
 using namespace std;
 /* header end */
 
-vector<pair<int, int>>interval;
+int minDelete(string s) {
+    int dp[500][500] = { 0 };
+    int length = s.size();
+    for (int i = 0; i < length; i++) {
+        for (int j = 0 ; j < length - i; j++) {
+            if (j == j + i) {
+                dp[j][j + i] = 0;
+                continue;
+            }
+            if (s[j] == s[j + i])
+                dp[j][j + i] = dp[j + 1][j + i - 1];
+            else
+                dp[j][j + i] = min(dp[j + 1][j + i], dp[j][j + i - 1]) + 1;
+
+        }
+    }
+    return dp[0][length - 1];
+}
 
 int main() {
-    int n;
-    cin >> n;
-    interval.resize(n);
-    for (auto &i : interval) cin >> i.first >> i.second;
-    sort(internal.begin(), internal.end());
+    string s;
+    while (cin >> s) cout << minDelete(s) << endl;
     return 0;
 }
