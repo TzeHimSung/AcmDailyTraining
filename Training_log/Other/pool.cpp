f8a1e02ff7610730218e3d41ebef062335dad19f
JHSeng <zxc98567@gmail.com>
2020-03-04 22:44:25 +0800

    update Boost pool object_pool

--- Training_log/Other/pool.cpp
+++ Training_log/Other/pool.cpp
@@ -1,22 +1,45 @@
 #include <boost/pool/pool.hpp>
-// #include <boost/pool/object_pool.hpp>
+#include <boost/pool/object_pool.hpp>
 #include <iostream>
 
 using namespace std;
 
 const int maxn = 10;
 
+class A {
+public:
+    int a;
+    A() {
+        a = 0;
+    }
+    ~A() {}
+};
+
 int main() {
     boost::pool<> p(sizeof(int));
-    int *a[maxn];
+    boost::object_pool<A> q;
+    int *num[maxn];
+    A *a[maxn];
+    // call pool
     for (int i = 0; i < maxn; i++) {
-        a[i] = static_cast<int *>(p.malloc());
-        *a[i] = i;
-        cout << *a[i] << endl;
+        num[i] = static_cast<int *>(p.malloc());
+        *num[i] = i;
+        cout << *num[i] << endl;
     }
     for (int i = 0; i < maxn; i++) {
-        p.free(a[i]);
-        a[i] = nullptr;
+        p.free(num[i]);
+        num[i] = nullptr;
     }
+    // call object pool
+    for (int i = 0; i < maxn; i++) {
+        a[i] = q.construct();
+        cout << i << "! ";
+    }
+    cout << endl;
+    for (int i = 0; i < maxn; i++) {
+        q.destroy(a[i]);
+        cout << i << "! ";
+    }
+    cout << endl;
     return 0;
 }
