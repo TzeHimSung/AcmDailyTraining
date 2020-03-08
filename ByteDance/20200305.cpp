a35bc0b2b3358a3b610121928e7c924254a1b515
JHSeng <zxc98567@gmail.com>
2020-03-08 20:47:44 +0800

    update ByteDance 20200305.cpp

---
+++ ByteDance/20200305.cpp
@@ -0,0 +1,19 @@
+/* basic header */
+#include <bits/stdc++.h>
+/* define */
+#define ll long long
+#define pb emplace_back
+#define mp make_pair
+#define eps 1e-8
+#define lson (curpos<<1)
+#define rson (curpos<<1|1)
+/* namespace */
+using namespace std;
+/* header end */
+
+// todo, have some idea
+
+int main() {
+    /* main body */
+    return 0;
+}
