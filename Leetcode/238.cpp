699e0e0f5a793c8fe9794a0cd70c4a6416963754
JHSeng <zxc98567@gmail.com>
2020-03-25 19:30:02 +0800

    update Leetcode

---
+++ Leetcode/238.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int> &nums) {
+        int left = 1, right = 1, n = nums.size();
+        vector<int>res(n, 1);
+        for (int i = 0; i < n; i++) {
+            res[i] *= left;
+            left *= nums[i];
+            res[n - i - 1] *= right;
+            right *= nums[n - i - 1];
+        }
+        return res;
+    }
+};
