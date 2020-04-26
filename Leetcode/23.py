a93ad91684654cb4b13fcc9a4a5d988ccc131b0e
TzeHimSung <zxc98567@163.com>
2020-04-26 22:54:49 +0800

    update Leetcode

--- Leetcode/23.py
+++ Leetcode/23.py
@@ -8,18 +8,18 @@
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         import heapq
-        dummy = ListNode(0)
-        p = dummy
-        head = []
+        ans = ListNode(0)
+        p = ans
+        queue = []
         for i in range(len(lists)):
             if lists[i]:
-                heapq.heappush(head, (lists[i].val, i))
+                heapq.heappush(queue, (lists[i].val, i))
                 lists[i] = lists[i].next
-        while head:
-            val, idx = heapq.heappop(head)
+        while queue:
+            val, idx = heapq.heappop(queue)
             p.next = ListNode(val)
             p = p.next
             if lists[idx]:
-                heapq.heappush(head, (lists[idx].val, idx))
+                heapq.heappush(queue, (lists[idx].val, idx))
                 lists[idx] = lists[idx].next
-        return dummy.next
+        return ans.next
