b600dfc7bfb1b3ccee40b11396ca61729f322623
TzeHimSung <zxc98567@163.com>
2020-04-27 22:30:30 +0800

    update Leetcode

---
+++ Leetcode/33.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int> &nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            // left is ordered
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) r = mid - 1;
+                else l = mid + 1;
+            } else { // right is ordered
+                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
+                else r = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
